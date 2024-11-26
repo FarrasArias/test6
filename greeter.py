58e2915553c7981c115fe6da34b3a39787f56a93
FarrasArias <rafael.arias.glez@gmail.com>
2024-11-25 17:52:45 -0800

    added bug

--- greeter.py
+++ greeter.py
@@ -1,6 +1,6 @@
 def greet(name, greeting="Hello"):
     """This function greets the person with a personalized greeting. Random Change"""
-    print(f"{greeting}, {name}!")
+    print("{greeting}, {name}!")
 
 if __name__ == "__main__":
     greet("World", "Howdy") 
