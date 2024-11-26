992b5274bf35a2841e19b35e769c830d58121e91
FarrasArias <rafael.arias.glez@gmail.com>
2024-11-25 17:34:07 -0800

    for new feature

--- greeter.py
+++ greeter.py
@@ -1,6 +1,6 @@
 def greet(name, greeting="Hello"):
     """This function greets the person with a personalized greeting."""
-    print(f"{greeting}, {name}!")
+    print(f"{greeting}, {name}! A")
 
 if __name__ == "__main__":
     greet("World", "Howdy") 
